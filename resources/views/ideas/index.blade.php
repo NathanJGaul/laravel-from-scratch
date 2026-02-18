f96fbb8569da4ee0beecfebd7c3c2d55f8246fa7
Nathan Gaul <nathanjgaul@gmail.com>
2026-02-17 18:45:56 -0600

    Implement IdeaController

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -1,31 +1,17 @@
 <x-layout>
-    <form method="POST" action="ideas">
-        @csrf
-        <div class="col-span-full">
-            <label for="description" class="block text-sm/6 font-medium text-white">New Idea</label>
-            <div class="mt-2">
-                <textarea id="description" name="description" rows="3"
-                    class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
-            </div>
-            <p class="mt-3 text-sm/6 text-gray-400">Have an idea you want to save for later?</p>
-        </div>
-        <div class="mt-6 flex items-center gap-x-6">
-            <button type="submit"
-                class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
-        </div>
-        </div>
-    </form>
-
     <div class="text-white">
         @if ($ideas->count())
             <h2 class="font-bold">Your Ideas</h2>
             <ul>
                 @foreach ($ideas as $idea)
-                    <a href="/ideas/{{ $idea->id }}" class="text-sm">{{ $idea->description }}</a>
+                    <li><a href="/ideas/{{ $idea->id }}" class="text-sm">{{ $idea->description }}</a></li>
                 @endforeach
             </ul>
+            <div class="pt-8">
+                <a href="/ideas/create" class="underline">Create a new idea.</a>
+            </div>
         @else
-            <p>Looks like you don't have any ideas.</p>
+            <p>Looks like you don't have any ideas. <a href="/ideas/create" class="underline">Create a new one.</a></p>
         @endif
     </div>
 </x-layout>
